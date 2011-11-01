10d6773370dfa8e196a9085e8a7034feced4b424
David King <dking@xforty.com>
2011-11-01 15:25:25 -0400

    Remove out-of-date cursor:hand style

--- js/uc_color_attribute.js
+++ js/uc_color_attribute.js
@@ -4,13 +4,14 @@ $(document).ready(function(){
 		var oId = $(this).attr('oid');
 		var exists = $('div#txtmsg').html();
 		var title = $(this).attr('title');
+    var text = Drupal.t('Selected Color');
 
 		$('#edit-attributes-'+attrId).val(oId).trigger('change');
 
 		if(exists){
-			$('div#txtmsg').html('<strong>Selected Colour:</strong> '+title);
+			$('div#txtmsg').html('<strong>'+text+':</strong> '+title);
 		}else{
-			$('div#colors-background').before('<div id="txtmsg"><strong>Selected Colour:</strong> '+title+'</div>');
+			$('div#colors-background').before('<div id="txtmsg"><strong>'+text+':</strong> '+title+'</div>');
 		}
 	});
 });
