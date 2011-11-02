b8a0f0370a4468a79fd6df0eb2a75723efd7abf4
David King <dking@xforty.com>
2011-11-02 09:24:08 -0400

    Convert to unix line endings and tabs to spaces

--- js/uc_color_attribute.js
+++ js/uc_color_attribute.js
@@ -1,17 +1,17 @@
 $(document).ready(function(){
-	$('div.colors-color').click(function() {
-		var attrId = $(this).attr('attrid');
-		var oId = $(this).attr('oid');
-		var exists = $('div#txtmsg').html();
-		var title = $(this).attr('title');
+  $('div.colors-color').click(function() {
+    var attrId = $(this).attr('attrid');
+    var oId = $(this).attr('oid');
+    var exists = $('div#txtmsg').html();
+    var title = $(this).attr('title');
     var text = Drupal.t('Selected Color');
 
-		$('#edit-attributes-'+attrId).val(oId).trigger('change');
+    $('#edit-attributes-'+attrId).val(oId).trigger('change');
 
-		if(exists){
-			$('div#txtmsg').html('<strong>'+text+':</strong> '+title);
-		}else{
-			$('div#colors-background').before('<div id="txtmsg"><strong>'+text+':</strong> '+title+'</div>');
-		}
-	});
+    if(exists){
+      $('div#txtmsg').html('<strong>'+text+':</strong> '+title);
+    }else{
+      $('div#colors-background').before('<div id="txtmsg"><strong>'+text+':</strong> '+title+'</div>');
+    }
+  });
 });
